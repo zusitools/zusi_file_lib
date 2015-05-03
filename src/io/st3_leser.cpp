565cca14e2427d357e5e4145c25202ed2bbb8ca9
Johannes <zusitools@web.de>
2015-05-03 22:14:41 +0200

    [St3-Leser] Lies Element-Koordinaten

--- src/io/st3_leser.cpp
+++ src/io/st3_leser.cpp
@@ -4,8 +4,25 @@
 
 #include "lib/rapidxml-1.13/rapidxml.hpp"
 
+void liesFloatAttr(xml_node<> &node, const char *name, float *wert) {
+    if (wert != nullptr)
+    {
+        xml_attribute<> *attr = node.first_attribute(name);
+        if (attr != nullptr)
+        {
+            *wert = stof(attr->value());
+        }
+    }
+}
+
+void liesXYZ(xml_node<> &node, float *x, float *y, float *z) {
+    liesFloatAttr(node, "X", x);
+    liesFloatAttr(node, "Y", y);
+    liesFloatAttr(node, "Z", z);
+}
+
 unique_ptr<Strecke> St3Leser::liesSt3Datei(istream& datei) {
-    unique_ptr<Strecke> result(new Strecke());
+    unique_ptr<Strecke> strecke(new Strecke());
 
     vector<char> buffer((istreambuf_iterator<char>(datei)), istreambuf_iterator<char>());
     buffer.push_back('\0');
@@ -16,8 +33,45 @@ unique_ptr<Strecke> St3Leser::liesSt3Datei(istream& datei) {
     xml_node<> *wurzel = dok.first_node("Zusi");
     if (wurzel != nullptr)
     {
-        result->dateiInfo = this->liesDateiInfo(*wurzel);
+        strecke->dateiInfo = this->liesDateiInfo(*wurzel);
     }
 
-    return result;
+    xml_node<> *str_node = wurzel->first_node("Strecke");
+    if (str_node != nullptr)
+    {
+        xml_node<> *elem_node = str_node->first_node("StrElement");
+        while (elem_node != nullptr)
+        {
+            shared_ptr<Streckenelement> element(new Streckenelement());
+
+            xml_attribute<> *nrAttr = elem_node->first_attribute("Nr");
+            if (nrAttr != nullptr) {
+                // TODO: Fehlerbehandlung
+                element->nr = stoi(nrAttr->value());
+            } else {
+                element->nr = 0;
+            }
+
+            if (strecke->streckenelemente.size() <= element->nr + 1)
+            {
+                strecke->streckenelemente.resize(element->nr + 1);
+            }
+            strecke->streckenelemente.at(element->nr) = element;
+
+            xml_node<> *g_node = elem_node->first_node("g");
+            if (g_node != nullptr)
+            {
+                liesXYZ(*g_node, &element->p1.x, &element->p1.y, &element->p1.z);
+            }
+            xml_node<> *b_node = elem_node->first_node("b");
+            if (b_node != nullptr)
+            {
+                liesXYZ(*b_node, &element->p2.x, &element->p2.y, &element->p2.z);
+            }
+
+            elem_node = elem_node->next_sibling("StrElement");
+        }
+    }
+
+    return strecke;
 }
--- test/st3_lese_test.cpp
+++ test/st3_lese_test.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+// Wert für Floating-Point-Vergleiche (in Prozent der zu vergleichenden Werte!)
+#define epsilon 0.00001
+
 BOOST_AUTO_TEST_CASE(leere_st3_datei) {
     ifstream infile("./eingabe/zusi3/LeereSt3Datei.st3");
     unique_ptr<Strecke> strecke = St3Leser().liesSt3Datei(infile);
@@ -15,4 +18,19 @@ BOOST_AUTO_TEST_CASE(leere_st3_datei) {
 
     BOOST_CHECK_EQUAL(strecke->dateiInfo->formatVersion, "A.1");
     BOOST_CHECK_EQUAL(strecke->dateiInfo->formatMinVersion, "A.1");
+}
+
+BOOST_AUTO_TEST_CASE(element_koordinaten) {
+    ifstream infile("./eingabe/zusi3/ElementKoordinatenTest.st3");
+    unique_ptr<Strecke> strecke = St3Leser().liesSt3Datei(infile);
+
+    BOOST_REQUIRE_EQUAL(strecke->streckenelemente.size(), 2);
+
+    auto& element = strecke->streckenelemente.at(1);
+    BOOST_CHECK_CLOSE(element->p1.x, -10, epsilon);
+    BOOST_CHECK_CLOSE(element->p1.y, -23.5, epsilon);
+    BOOST_CHECK_CLOSE(element->p1.z, 5, epsilon);
+    BOOST_CHECK_CLOSE(element->p2.x, 10, epsilon);
+    BOOST_CHECK_CLOSE(element->p2.y, 20, epsilon);
+    BOOST_CHECK_CLOSE(element->p2.z, -30, epsilon);
 }
