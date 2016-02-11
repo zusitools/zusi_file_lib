27e98c387a6b7ddb71680c42975cd1fd95146533
Johannes <zusitools@web.de>
2016-02-11 16:34:45 +0100

    Lies Zusi-3-Strecken in Test fuer offizielle Strecken

--- test/zusi_offizielle_strecken_test.cpp
+++ test/zusi_offizielle_strecken_test.cpp
@@ -5,10 +5,11 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include <io/str_leser.hpp>
+#include <io/st3_leser.hpp>
 
 using namespace std;
 
-BOOST_AUTO_TEST_CASE(lies_strecken) {
+BOOST_AUTO_TEST_CASE(lies_z2_strecken) {
     vector<string> strDateiNamen({
         "Arnstadt_Ilmenau/Arnstadt_Ilmenau.str",
         "Bln_Anhalter_Bhf_Jueterbog/Strecke.str",
@@ -99,7 +100,7 @@ BOOST_AUTO_TEST_CASE(lies_strecken) {
         "LinkeRheinstrecke/LinkeRheinstrecke1987_V2_62.str",
     });
 
-    char *zusiStreckenPfad = getenv("ZUSI_STRECKEN_PFAD");
+    char *zusiStreckenPfad = getenv("ZUSI2_STRECKEN_PFAD");
     BOOST_REQUIRE(zusiStreckenPfad != nullptr);
 
     for (const string& dateiname : strDateiNamen) {
@@ -107,6 +108,87 @@ BOOST_AUTO_TEST_CASE(lies_strecken) {
         ifstream infile(zusiStreckenPfad + dateiname);
         auto leseStart = chrono::high_resolution_clock::now();
         unique_ptr<Strecke> strecke = StrLeser().liesStrDatei(infile);
+        auto leseEnde = chrono::high_resolution_clock::now();
+        auto ms = chrono::duration_cast<chrono::milliseconds>(leseEnde - leseStart).count();
+        BOOST_MESSAGE(dateiname + ": " + to_string(ms) + " ms");
+    }
+}
+
+BOOST_AUTO_TEST_CASE(lies_z3_strecken) {
+    vector<string> strDateiNamen({
+        "Deutschland/32T_0005_0053/000463_005293_Grimmelshofen/Grimmelshofen_2008.st3",
+        "Deutschland/32T_0005_0053/000465_005296_Fuetzen/fuetzen2005.st3",
+        "Deutschland/32T_0005_0053/000468_005299_Zollhaus/Zollhaus2008.st3",
+        "Deutschland/32U_0004_0057/000442_005692_Freienohl/Freienohl_1985.st3",
+        "Deutschland/32U_0004_0057/000444_005689_Wennemen/Wennemen_1973.st3",
+        "Deutschland/32U_0004_0057/000449_005689_Meschede/Meschede_1980.st3",
+        "Deutschland/32U_0005_0057/000455_005689_Eversberg/Eversberg_1985.st3",
+        "Deutschland/32U_0005_0057/000460_005690_Bestwig/Bestwig_1985.st3",
+        "Deutschland/32U_0005_0057/000461_005691_Nuttlar/Nuttlar_1980.st3",
+        "Deutschland/32U_0005_0057/000464_005690_Olsberg/Olsberg_1973.st3",
+        "Deutschland/32U_0005_0057/000466_005689_Elleringhausen/Elleringhausen_1980.st3",
+        "Deutschland/32U_0005_0057/000471_005689_Brilon_Wald/Brilon_Wald_1985.st3",
+        "Deutschland/32U_0005_0057/000475_005692_Hoppecke/Hoppecke_1980.st3",
+        "Deutschland/32U_0005_0057/000480_005694_Messinghausen/Messinghausen_1980.st3",
+        "Deutschland/32U_0005_0057/000483_005730_Paderborn/Paderborn_1985.st3",
+        "Deutschland/32U_0005_0057/000486_005697_Bredelar/Bredelar_1978.st3",
+        "Deutschland/32U_0005_0057/000487_005731_Benhausen/Benhausen_1985.st3",
+        "Deutschland/32U_0005_0057/000490_005701_Marsberg/Marsberg_1978.st3",
+        "Deutschland/32U_0005_0057/000495_005705_Westheim/Westheim_1985.st3",
+        "Deutschland/32U_0005_0057/000496_005734_Altenbeken/Altenbeken_1985.st3",
+        "Deutschland/32U_0005_0057/000497_005731_Buke/Buke_1985.st3",
+        "Deutschland/32U_0005_0057/000498_005723_Neuenheerse/Neuenheerse_1985.st3",
+        "Deutschland/32U_0005_0057/000499_005737_Langeland/Langeland_1985.st3",
+        "Deutschland/32U_0005_0057/000500_005707_Wrexen/Wrexen_1985.st3",
+        "Deutschland/32U_0005_0057/000500_005719_Willebadessen/Willebadessen_1985.st3",
+        "Deutschland/32U_0005_0057/000500_005739_Himmighausen/Himmighausen_1985.st3",
+        "Deutschland/32U_0005_0057/000503_005732_Driburg/Driburg_1985.st3",
+        "Deutschland/32U_0005_0057/000504_005709_Scherfede/Scherfede_1974.st3",
+        "Deutschland/32U_0005_0057/000505_005712_Bonenburg/Bonenburg_1985.st3",
+        "Deutschland/32U_0005_0057/000509_005707_Noerde/Noerde_1985_Dummy.st3",
+        "Deutschland/32U_0005_0057/000509_005707_Warburg/Warburg_1985.st3",
+        "Deutschland/32U_0005_0057/000517_005705_Liebenau/Liebenau_1980.st3",
+        "Deutschland/32U_0005_0057/000523_005708_Lamerden/Lamerden_1985.st3",
+        "Deutschland/32U_0005_0057/000527_005706_Hofgeismar/Hofgeismar_1985.st3",
+        "Deutschland/32U_0005_0057/000527_005709_Huemme/Huemme_1980.st3",
+        "Deutschland/32U_0005_0057/000530_005701_Grebenstein/Grebenstein_1985.st3",
+        "Deutschland/32U_0005_0057/000531_005684_Kassel_Wilhelmshoehe/Kassel_Wilhelmshoehe_1988.st3",
+        "Deutschland/32U_0005_0057/000531_005690_Obervellmar/Obervellmar_1985.st3",
+        "Deutschland/32U_0005_0057/000531_005696_Immenhausen/Immenhausen_1985.st3",
+        "Deutschland/32U_0005_0057/000532_005686_Kassel_Bahndreieck/Kassel_Bahndreieck_1990.st3",
+        "Deutschland/32U_0005_0057/000532_005693_Moenchehof/Moenchehof_1980.st3",
+        "Deutschland/32U_0005_0057/000533_005686_Kassel_Hbf/Kassel_Hbf_1988.st3",
+        "Deutschland/32U_0005_0057/000533_005687_Harleshausen/Harleshausen_1980.st3",
+        "Deutschland/32U_0005_0057/000533_005687_Kassel_Rbf/Kassel_Rbf_1988.st3",
+        "Deutschland/32U_0005_0057/000534_005686_Kassel_Unterstadt/Kassel_Unterstadt_GF_Dummy.st3",
+        "Deutschland/32U_0005_0057/000534_005689_Niedervellmar/Niedervellmar_2000.st3",
+        "Deutschland/32U_0005_0057/000537_005690_Ihringshausen/Ihringshausen_2000.st3",
+        "Deutschland/32U_0005_0057/000541_005691_Lutterberg/Lutterberg_2000.st3",
+        "Deutschland/32U_0005_0057/000548_005695_Lippoldshausen/Lippoldshausen_2000.st3",
+        "Deutschland/32U_0005_0060/000486_006019_StPeterOrding/StPeterOrding_2006.st3",
+        "Deutschland/32U_0005_0060/000500_006026_Hoern-Toenning/Hoern-Toenning_2008.st3",
+        "Deutschland/32U_0005_0060/000504_006026_Friedrichstadt/Friedrichstadt_2006.st3",
+        "Deutschland/32U_0005_0060/000504_006031_AbzwHoern/AbzwHoern_2006.st3",
+        "Deutschland/32U_0005_0060/000504_006037_Husum/Husum_2006.st3",
+        "Deutschland/32U_0006_0057/000558_005703_Juehnde/Juehnde_2000.st3",
+        "Deutschland/32U_0006_0057/000562_005708_Siekweg/Siekweg_2000.st3",
+        "Deutschland/32U_0006_0057/000564_005711_Goettingen_Pbf/Goettingen_Pbf_2000.st3",
+        "Deutschland/32U_0006_0057/000563_005706_Rosdorf/Rosdorf_1998.st3",
+        "_Docu/Bremsteststrecke/Bremsteststrecke.st3",
+        "_Docu/Kap3_Uebungsstrecke/000442_005680_Eslohe/Eslohe_1980.st3",
+        "_Docu/Kap3_Uebungsstrecke/000443_005681_Wenholthausen/Wenholthausen_1980.st3",
+        "_Docu/Kap3_Uebungsstrecke/000443_005681_Wenholthausen/Wenholthausen_1980_Grundplatte_Huellkurve.st3",
+        "_Docu/Radienteststrecke/Radienteststrecke.st3",
+    });
+
+    char *zusiStreckenPfad = getenv("ZUSI3_STRECKEN_PFAD");
+    BOOST_REQUIRE(zusiStreckenPfad != nullptr);
+
+    for (const string& dateiname : strDateiNamen) {
+        BOOST_TEST_CHECKPOINT(dateiname);
+        ifstream infile(zusiStreckenPfad + dateiname);
+        auto leseStart = chrono::high_resolution_clock::now();
+        unique_ptr<Strecke> strecke = St3Leser().liesSt3Datei(infile);
         auto leseEnde = chrono::high_resolution_clock::now();
         auto ms = chrono::duration_cast<chrono::milliseconds>(leseEnde - leseStart).count();
         BOOST_MESSAGE(dateiname + ": " + to_string(ms) + " ms");
