a5741009e3a51fa24e841076e87734020076cb1e
Johannes <zusitools@web.de>
2017-03-01 14:45:23 +0100

    [Z2Leser] Nutze boost::spirit::qi zum Parsen von Zahlen

--- src/io/z2_leser.cpp
+++ src/io/z2_leser.cpp
@@ -3,9 +3,25 @@
 #endif
 #include <stdexcept>
 
+#include <boost/spirit/include/qi_real.hpp>
+#include <boost/spirit/include/qi_int.hpp>
+
 #include <io/z2_leser.hpp>
 
 using namespace std;
+namespace qi = boost::spirit::qi;
+
+template <typename T>
+struct decimal_comma_real_policies : qi::real_policies<T>
+{
+    template <typename Iterator> static bool parse_dot(Iterator& first, Iterator const& last)
+    {
+        if (first == last || *first != ',')
+            return false;
+        ++first;
+        return true;
+    }
+};
 
 string Z2Leser::liesZeile(istream& datei) {
     string result;
@@ -37,16 +53,12 @@ int_fast32_t Z2Leser::liesGanzzahl(const string& aktElement, istream& datei) {
 }
 
 int_fast32_t Z2Leser::konvertiereInGanzzahl(string zeile) {
-    if (zeile.find_first_not_of("-0123456789") != string::npos) {
+    auto begin = zeile.begin(), end = zeile.end();
+    int_fast32_t result;
+    if (!qi::parse(begin, end, qi::int_, result) || (begin != end)) {
         throw invalid_argument("'" + zeile + "' kann nicht in eine Ganzzahl konvertiert werden.");
-    } else {
-        try {
-            return stoi(zeile);
-        } catch (invalid_argument) {
-            throw invalid_argument(
-                "'" + zeile + "' kann nicht in eine Ganzzahl konvertiert werden.");
-        }
     }
+    return result;
 }
 
 int_fast32_t Z2Leser::konvertiereInGanzzahl(const string& aktElement, string zeile) {
@@ -64,20 +76,13 @@ double Z2Leser::liesGleitkommazahl(const string& aktElement, istream& datei) {
 }
 
 double Z2Leser::konvertiereInGleitkommazahl(string zeile) {
-    if (zeile.find_first_not_of("-,0123456789eE") != string::npos) {
-        throw invalid_argument(zeile + " kann nicht in eine Gleitkommazahl konvertiert werden.");
-    } else {
-        // Ersetze (erstes) , durch .
-        size_t kommaPos = zeile.find(',');
-        if (kommaPos != string::npos) {
-            zeile[kommaPos] = '.';
-        }
-        if (zeile.find(',') != string::npos) {
-            throw invalid_argument(
-                zeile + " kann nicht in eine Gleitkommazahl konvertiert werden.");
-        }
-        return stod(zeile);
+    auto begin = zeile.begin(), end = zeile.end();
+    double result;
+    qi::real_parser<double, decimal_comma_real_policies<double> > parser;
+    if (!qi::parse(begin, end, parser, result) || (begin != end)) {
+        throw invalid_argument("'" + zeile + "' kann nicht in eine Gleitkommazahl konvertiert werden.");
     }
+    return result;
 }
 
 double Z2Leser::konvertiereInGleitkommazahl(const string& aktElement, string zeile) {
